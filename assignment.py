3de61e1d81953eb2fdcf21c0b4427b4cf1c63fa2
Afzal Shahul Hameed <aflibnush@gmail.com>
2020-08-21 02:51:43 +0530

    Initial commit

---
+++ assignment.py
@@ -0,0 +1,12 @@
+"""
+customerdata.txt contains transactional data from a fictitious website.
+Parse it's data and generate a report with the following information
+
+1. How many orders did the site receive?
+2. What was the total amount of the orders?
+3. List the names of the customers who ordered once and did not order again.
+4. Get a distribution of customers who ordered exactly once, exactly twice,
+  and so on up to 4 orders and group the rest as 5 orders and above
+5. Generate this report as a simple HTML page with a table.
+6. Add a bar graph for the information in Q4 in your HTML report.
+"""
